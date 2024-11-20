21cf08c9e777dba49f3b6262504998dc84bc3cb3
Magdalena <magdalena.parks@wearelevels.com>
2024-11-20 16:38:00 +0100

    update eslint to work with typescript

--- backend/.eslintrc.cjs
+++ backend/.eslintrc.cjs
@@ -2,7 +2,15 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: 'eslint:recommended',
+  parser: "@typescript-eslint/parser",
+  plugins: [
+    "@typescript-eslint"
+  ],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
