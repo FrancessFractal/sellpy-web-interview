6ad5856e47e4849db43c6093afaad808f4abb429
Magdalena <magdalena.parks@wearelevels.com>
2024-11-20 11:25:19 +0100

    install ts on backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,6 +8,8 @@ app.use(express.json())
 
 const PORT = 3001
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
